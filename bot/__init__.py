14b4f1c397c1050053344fd73b5c7f23e81a9669
realaashil <singhalaashil@gmail.com>
2024-04-09 16:20:45 +0530

    Bot: Include events router in main router

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,5 +1,6 @@
 from aiogram import Router
 
+from bot.events.server import events_router
 from bot.handler.admin import admin_router
 from bot.handler.callbacks import callback_router
 from bot.handler.commands import commands_router
@@ -8,5 +9,6 @@ main_router = Router()
 main_router.include_router(admin_router)
 main_router.include_router(commands_router)
 main_router.include_router(callback_router)
+main_router.include_router(events_router)
 
 __all__ = ("main_router",)
